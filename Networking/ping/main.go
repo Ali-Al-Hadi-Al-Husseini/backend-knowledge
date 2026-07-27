f2d1557ed55bf530df9462f55771a31fc5e46985
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-07-27 10:30:45 +0300

    added arguments to ping

--- Networking/ping/main.go
+++ Networking/ping/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
+func main() {
+	err := Ping(os.Args[1], 10, 1*time.Second)
+	if err != nil {
+		fmt.Println(fmt.Errorf("ping error: %w", err))
+	}
+}
+
+func Ping(dest string, attempts int, delay time.Duration) error {
+
+	return nil
 }
