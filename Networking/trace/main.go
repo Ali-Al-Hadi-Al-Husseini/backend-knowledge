cd85f5aaedbd52d3e303e73177e96913a335446e
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-07-29 09:14:38 +0300

    added mod file

--- Networking/trace/main.go
+++ Networking/trace/main.go
@@ -1,15 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	err := trace()
+	err := trace(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func trace() error {
+func establishConn(target string) (*net.IPConn, error) {
 
+	raddres, err := net.ResolveIPAddr("ip4", target)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to resolve target: %w", target)
+	}
+	conn, err := net.DialIP("ip4", nil, raddres)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to dial ip: %w", raddres)
+	}
+
+	return conn, nil
+}
+func trace(target string) error {
+
+	conn, err := establishConn(target)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
 	return nil
 }
