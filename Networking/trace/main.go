d83e5dd1dd58586c5029941b45cd158c3c3342e2
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-08-02 15:34:21 +0300

    wrote to the server

--- Networking/trace/main.go
+++ Networking/trace/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
+	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 )
 
 func main() {
-	err := trace(os.Args[1], 30)
+	err := trace(os.Args[1], 64)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -37,7 +39,32 @@ func trace(target string, hops int) error {
 	defer conn.Close()
 	data := []byte("hola amigo")
 
+	fmt.Printf("traceroute to %s (%s), %d hops max, 40 byte packets\n", target, raddres, hops)
 	for ttl := 1; ttl <= hops; ttl++ {
+		conn.SetTTL(ttl)
+
+		echoReq := icmp.Message{
+			Type: ipv4.ICMPTypeEcho,
+			Code: 0,
+			Body: &icmp.Echo{
+				ID:   os.Getpid() & 0xffff,
+				Seq:  ttl,
+				Data: data[:],
+			},
+		}
+		message, err := echoReq.Marshal(nil)
+		if err != nil {
+			return fmt.Errorf("unable to Marshal message")
+		}
+		err = conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+		if err != nil {
+			return fmt.Errorf("unavle to readdead line")
+		}
+
+		_, err = conn.WriteTo(message, nil, raddres)
+		if err != nil {
+			return fmt.Errorf("server unreachable")
+		}
 
 	}
 	return nil
