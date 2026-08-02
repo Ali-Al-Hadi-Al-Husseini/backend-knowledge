73769bb84ffd47996c1a343250d5b18cd3009a77
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-08-02 15:07:56 +0300

    wrapped conn with ipv4.PacketConn

--- Networking/trace/main.go
+++ Networking/trace/main.go
@@ -4,35 +4,41 @@ import (
 	"fmt"
 	"net"
 	"os"
+
+	"golang.org/x/net/ipv4"
 )
 
 func main() {
-	err := trace(os.Args[1])
+	err := trace(os.Args[1], 30)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func establishConn(target string) (*net.IPConn, error) {
+func establishConn(target string) (*ipv4.PacketConn, *net.IPAddr, error) {
 
 	raddres, err := net.ResolveIPAddr("ip4", target)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to resolve target: %w", target)
+		return nil, nil, fmt.Errorf("Unable to resolve %s: %w", target, err)
 	}
-	conn, err := net.DialIP("ip4", nil, raddres)
+	conn, err := net.ListenIP("ip4:icmp", nil)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to dial ip: %w", raddres)
+		return nil, raddres, fmt.Errorf("Unable to establish listener: %w", err)
 	}
-
-	return conn, nil
+	return ipv4.NewPacketConn(conn), raddres, nil
 }
-func trace(target string) error {
+func trace(target string, hops int) error {
 
-	conn, err := establishConn(target)
+	conn, raddres, err := establishConn(target)
 	if err != nil {
 		return err
 	}
 
 	defer conn.Close()
+	data := []byte("hola amigo")
+
+	for ttl := 1; ttl <= hops; ttl++ {
+
+	}
 	return nil
 }
